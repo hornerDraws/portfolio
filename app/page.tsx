2b0279d87ccd99ac5cfa151f9b0ff26d6c3d77c5
hornerDraws <jannik.horner@gmail.com>
2026-03-03 14:33:18 +0100

    Create main gallery page

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const GalleryPage = () => {
+    return (
+        <div>
+            <h1>Gallery</h1>
+            <p>Welcome to the gallery page. Here are some of our works.</p>
+            <div className="gallery">
+                {/* Gallery items will be added here */}
+            </div>
+        </div>
+    );
+};
+
+export default GalleryPage;
